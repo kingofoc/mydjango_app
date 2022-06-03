c40ba7de8aad7606d158eba79db4919fcec56cba
kingofoc <kingofoc202@gmail.com>
2022-06-03 10:06:01 -0700

    initial commit

---
+++ project/Scripts/Chukwuebuka_Kingsley/Chukwuebuka_Kingsley/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Chukwuebuka_Kingsley project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chukwuebuka_Kingsley.settings')
+
+application = get_asgi_application()
